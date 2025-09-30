d238fccb11d8072fcd68bc182ab1d7f4166080b9
diegoengelhard <00356119@uca.edu.sv>
2025-09-29 19:00:28 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hazard-Identifier-API
+A Node.js API for programmatic waste classification. The Hazard Identifier uses a weighted scoring system and a custom lexicon to analyze booking details (descriptions, products, notes) and flag potentially hazardous materials. A lightweight solution designed for high accuracy and easy integration.
---
+++ backend/app.ts
@@ -0,0 +1,26 @@
+require('dotenv').config();
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import indexRouter from './src/routes/index';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+
+app.use('/', indexRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+export default app;
